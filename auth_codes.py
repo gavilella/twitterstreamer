dd735f5aa462d29c0d41f80003d9330ca20c8d35
gavilella <vilella.gabriel@gmail.com>
2019-06-05 17:25:22 -0300

    Update auth_codes.py

--- auth_codes.py
+++ auth_codes.py
@@ -1,5 +1,5 @@
 # Authentication keys and tokens
-consumer_token = 'wlxvZiidCAXtggQj3PrhG77hF'
-consumer_secret = 'kbViswFEUacputdX0LcYcJnuKC9zOCqhUBGT2hqa0BKkYjMo4D'
-access_token = '51298679-hUgN90HleLfezDKB2EOTZyTiNNA8UQMgOZTPMwaiS'
-access_secret = 'T67gT8kNDwdOWSdAa7IlQofnN9O2gyZ3UgSmaDi3WThPU'
+consumer_token = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
